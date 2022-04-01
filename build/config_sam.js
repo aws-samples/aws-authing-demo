bf86a62e3c66019cdf686f7a010b29e575f255a3
yunnysunny <yunnysunny@gmail.com>
2022-04-01 14:39:22 +0800

    完善oidc api示例

--- build/config_sam.js
+++ build/config_sam.js
@@ -3,5 +3,7 @@ const ENV = process.env;
 
 writeTo('../samconfig.toml.ejs', {
   aws_bucket_name_deploy: ENV.AWS_BUCKET_NAME_DEPLOY,
-  aws_region: ENV.AWS_DEFAULT_REGION
+  aws_region: ENV.AWS_DEFAULT_REGION,
+  authing_app_id: ENV.AUTHING_APP_ID,
+  authing_app_issuer: `https://${ENV.AUTHING_APP_NAME}.authing.cn/oidc`,
 }, '../samconfig.toml');
